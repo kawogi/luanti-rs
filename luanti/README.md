4b3d966a19a13ac2989168143086ec7c222bebaa
kawogi <github@kai.giebeler.de>
2025-03-24 04:09:34 +0100

    create luanti as meta-package

---
+++ luanti/README.md
@@ -0,0 +1,3 @@
+# luanti
+
+This is a meta-package which re-exports all luanti-rs libraries.
---
+++ luanti/src/lib.rs
@@ -0,0 +1,4 @@
+//! Re-export all luanti-rs libraries
+
+pub use luanti_protocol as protocol;
+pub use luanti_protocol_derive as protocol_derive;
