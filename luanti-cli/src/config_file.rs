b64a65b918418f72193d470739adf03ac9875dd8
kawogi <github@kai.giebeler.de>
2026-02-28 23:34:33 +0100

    create draft of luanti.cli

---
+++ luanti-cli/src/config_file.rs
@@ -0,0 +1,233 @@
+// TODO this entire parser is in draft state. Missing features are: lossless re-write, all sorts of tests
+
+use std::{
+    fmt::Display,
+    fs,
+    io::{BufRead, BufReader},
+    mem,
+    path::PathBuf,
+};
+
+use anyhow::{Result, bail};
+use flexstr::SharedStr;
+
+#[derive(Default)]
+struct ConfigFile {
+    path: Option<PathBuf>,
+    config: Config,
+}
+
+impl ConfigFile {
+    pub fn load(path: PathBuf) -> Result<Self> {
+        let reader = fs::File::open(&path)?;
+        let reader = BufReader::new(reader);
+
+        let mut config_builder = ConfigBuilder::default();
+        for line in reader.lines() {
+            let line = line?;
+            config_builder.parse_line(&line)?;
+        }
+        let config = config_builder.finish()?;
+
+        Ok(Self {
+            path: Some(path),
+            config,
+        })
+    }
+
+    // pub fn path(&self) -> Option<&PathBuf> {
+    //     self.path.as_ref()
+    // }
+}
+
+#[derive(Default)]
+struct ConfigBuilder {
+    config: Config,
+    prelude: Vec<String>,
+    state: ConfigBuilderState,
+    termination_tag: Option<SharedStr>,
+}
+
+#[derive(Default)]
+enum ConfigBuilderState {
+    #[default]
+    Default,
+    Section {
+        key: SharedStr,
+        builder: Box<ConfigBuilder>,
+    },
+    Multiline {
+        key: SharedStr,
+        multiline: String,
+    },
+    Complete,
+}
+
+impl ConfigBuilder {
+    fn new(level: u32) -> Self {
+        Self {
+            config: Config::new(level),
+            prelude: Vec::new(),
+            state: ConfigBuilderState::Default,
+            termination_tag: None,
+        }
+    }
+
+    fn parse_line(&mut self, line: &str) -> Result<bool> {
+        let trimmed = line.trim();
+
+        self.state = match mem::take(&mut self.state) {
+            ConfigBuilderState::Default => {
+                if trimmed.is_empty() || trimmed.starts_with('#') {
+                    self.prelude.push(line.to_owned());
+                    ConfigBuilderState::Default
+                } else if self.termination_tag.as_deref() == Some(trimmed) {
+                    if !self.prelude.is_empty() {
+                        let item = ConfigItem {
+                            prelude: mem::take(&mut self.prelude),
+                            key_value: None,
+                        };
+                        self.config.items.push(item);
+                    }
+                    ConfigBuilderState::Complete
+                } else {
+                    let Some((key, value)) = trimmed.split_once('=') else {
+                        return Err(anyhow::anyhow!("Invalid config line: {}", line));
+                    };
+                    let key = key.trim();
+                    let value = value.trim();
+
+                    if value == "{" {
+                        ConfigBuilderState::Section {
+                            key: key.into(),
+                            builder: Box::new(Self::new(self.config.depth + 1)),
+                        }
+                    } else {
+                        let item = ConfigItem {
+                            prelude: mem::take(&mut self.prelude),
+                            key_value: Some((key.into(), ConfigValue::String(value.into()))),
+                        };
+                        self.config.items.push(item);
+                        ConfigBuilderState::Default
+                    }
+                }
+            }
+            ConfigBuilderState::Section { key, mut builder } => {
+                if builder.parse_line(line)? {
+                    let item = ConfigItem {
+                        prelude: mem::take(&mut self.prelude),
+                        key_value: Some((key, ConfigValue::Group(builder.finish()?))),
+                    };
+                    self.config.items.push(item);
+                    ConfigBuilderState::Default
+                } else {
+                    ConfigBuilderState::Section { key, builder }
+                }
+            }
+            ConfigBuilderState::Multiline { key, mut multiline } => {
+                if line == r#"""""# {
+                    let item = ConfigItem {
+                        prelude: mem::take(&mut self.prelude),
+                        key_value: Some((key.clone(), ConfigValue::String(multiline.into()))),
+                    };
+                    self.config.items.push(item);
+                    ConfigBuilderState::Default
+                } else {
+                    if !multiline.is_empty() {
+                        multiline.push('\n');
+                    }
+                    multiline.push_str(line);
+                    ConfigBuilderState::Multiline { key, multiline }
+                }
+            }
+            ConfigBuilderState::Complete => {
+                bail!("Unexpected line after completion");
+            }
+        };
+
+        Ok(matches!(self.state, ConfigBuilderState::Complete))
+    }
+
+    fn finish(mut self) -> Result<Config> {
+        match self.state {
+            ConfigBuilderState::Default => {
+                if !self.prelude.is_empty() {
+                    let item = ConfigItem {
+                        prelude: mem::take(&mut self.prelude),
+                        key_value: None,
+                    };
+                    self.config.items.push(item);
+                }
+            }
+            ConfigBuilderState::Section { key, builder } => {
+                bail!(
+                    "missing termination tag for group value of `{key}`: '{}'",
+                    builder.termination_tag.unwrap_or_default()
+                );
+            }
+            ConfigBuilderState::Multiline { key, .. } => {
+                bail!(r#"missing termination tag for multiline value of `{key}`: '"""'"#);
+            }
+            ConfigBuilderState::Complete => {}
+        }
+
+        Ok(self.config)
+    }
+}
+
+#[derive(Default)]
+struct Config {
+    items: Vec<ConfigItem>,
+    depth: u32,
+}
+
+impl Config {
+    fn new(depth: u32) -> Self {
+        Self {
+            items: Vec::new(),
+            depth,
+        }
+    }
+}
+
+impl Display for Config {
+    fn fmt(&self, formatter: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        for item in &self.items {
+            item.fmt(formatter)?;
+        }
+        Ok(())
+    }
+}
+
+struct ConfigItem {
+    prelude: Vec<String>,
+    key_value: Option<(SharedStr, ConfigValue)>,
+    // indent: u32,
+}
+
+impl Display for ConfigItem {
+    fn fmt(&self, formatter: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        for line in &self.prelude {
+            formatter.write_str(line)?;
+        }
+        if let Some((key, value)) = &self.key_value {
+            write!(formatter, "{key} = ")?;
+            match value {
+                ConfigValue::String(str) => writeln!(formatter, "{str}")?,
+                ConfigValue::Group(group) => {
+                    writeln!(formatter, "{{")?;
+                    for item in &group.items {
+                        item.fmt(formatter)?;
+                    }
+                    writeln!(formatter, "}}")?;
+                }
+            }
+        }
+        Ok(())
+    }
+}
+
+enum ConfigValue {
+    String(SharedStr),
+    Group(Config),
+}
---
+++ luanti-cli/src/main.rs
@@ -0,0 +1,100 @@
+//! This crate provides some command line tools for administering a Luanti server.
+
+mod config_file;
+
+use std::{
+    env,
+    path::{Path, PathBuf},
+};
+
+use log::{LevelFilter, debug, error};
+
+const CONFIG_FILE_NAME: &str = "minetest.conf";
+const GAME_CONFIG_FILE_NAME: &str = "game.conf";
+const GAMES_DIR_NAME: &str = "games";
+const WORLDS_DIR_NAME: &str = "worlds";
+
+// further reading:
+// Look into `subgames.cpp/findSubgame` for the search algorithm used by Luanti to find the game.
+
+fn main() {
+    env_logger::Builder::from_default_env()
+        .filter_level(LevelFilter::Trace)
+        .init();
+
+    // minetest.conf
+    // server.conf
+    // mod.conf
+    // game.conf
+    // modpack.conf
+
+    let Some(install_location) = find_install_config() else {
+        error!("no installation (minetest.conf) found in current directory or parent directories");
+        return;
+    };
+}
+
+fn find_install_config() -> Option<PathBuf> {
+    debug!("searching for installation in current directory …");
+    match env::current_dir().as_deref() {
+        Ok(mut current_dir) => loop {
+            debug!("searching for installation in {}", current_dir.display());
+            let local_config = current_dir.join(CONFIG_FILE_NAME);
+            if local_config.is_file() {
+                debug!("found installation at {}", local_config.display());
+                return Some(local_config);
+            }
+            let Some(parent) = current_dir.parent() else {
+                debug!("no installation found in local directory");
+                return None;
+            };
+            current_dir = parent;
+        },
+        Err(error) => {
+            debug!("could not determine current working directory: {error}");
+            None
+        }
+    }
+}
+
+fn check_installation_root(path: &Path) -> Result<PathBuf, String> {
+    let config_path = path.join(CONFIG_FILE_NAME);
+    if !config_path.is_file() {
+        return Err(format!(
+            "{CONFIG_FILE_NAME} not found in {path}",
+            path = path.display()
+        ));
+    }
+
+    // games sometimes contain a minetest.conf file, which needs to be ignored for searching an installation's root
+    let game_config_path = path.join(GAME_CONFIG_FILE_NAME);
+    if game_config_path.is_file() {
+        return Err(format!(
+            "{GAME_CONFIG_FILE_NAME} found in {path}",
+            path = path.display()
+        ));
+    }
+
+    // TODO this might not be the case for newly created servers
+    // usually a `games` subdirectory needs to exist next to a `minetest.conf` file
+    let games_path = path.join(GAMES_DIR_NAME);
+    if !games_path.is_dir() {
+        return Err(format!(
+            "{GAMES_DIR_NAME} not found in {path}",
+            path = path.display()
+        ));
+    }
+
+    // TODO this might not be the case for newly created servers
+    // usually a `worlds` subdirectory needs to exist next to a `minetest.conf` file
+    let worlds_path = path.join(WORLDS_DIR_NAME);
+    if !worlds_path.is_dir() {
+        return Err(format!(
+            "{WORLDS_DIR_NAME} not found in {path}",
+            path = path.display()
+        ));
+    }
+
+    debug!("found installation root at {}", path.display());
+    Ok(config_path)
+}
